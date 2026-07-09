5d0a85890f949b1147275832dad8d572b6bf47d2
Shazia Maqsood <shaziaarain513@gmail.com>
2026-07-09 22:42:19 +0500

    Improve website UI and fix database connection

--- database.py
+++ database.py
@@ -1,11 +1,24 @@
 import os
 import mysql.connector
 
+
 def get_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+
+    if os.getenv("DB_HOST"):
+
+        return mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+
+    else:
+
+        return mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="job_portal"
+        )
