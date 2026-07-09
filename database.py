24cae5dbd8a7d3a2c3feed52532a4a51561f2638
Shazia Maqsood <shaziaarain513@gmail.com>
2026-07-09 22:19:57 +0500

    Update database configuration

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
+import os
 import mysql.connector
 
 def get_connection():
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="job_portal"
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
-
-    return connection
